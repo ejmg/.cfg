56334d59f2a3418867cb81096922f11c31832cdb
elias <elias.jm.garcia@gmail.com>
2017-11-08 18:34:47 -0600

    got init.el to tangle my org file, debugging now, beware mess

--- .myemacs.d/my-configs/my-init.el
+++ .myemacs.d/my-configs/my-init.el
@@ -1,12 +1,15 @@
+(defun tangle-init ()
+  "If the current buffer is 'init.org' the code-blocks are
+tangled, and the tangled file is compiled."
+  (when (equal (buffer-file-name)
+               (expand-file-name (concat user-emacs-directory "init.org")))
+    ;; Avoid running hooks when tangling.
+    (let ((prog-mode-hook nil))
+      (org-babel-tangle)
+      (byte-compile-file (concat user-emacs-directory "init.el")))))
 
-;; open my configuration file
-(find-file (concat default-directory "my-init.org"))
+(add-hook 'after-save-hook 'tangle-init)
 
-;; "tangle it", extracting my source code blocks
-(org-babel-tangle)
-
-;; load it, goodbye this file!
-(load-file (concat default-directory "my-init.el"))
-
-;; byte compilation
-(byte-compile-file (concat default-directory "my-init.el"))
+(use-package ivy :ensure t
+:diminish (ivy-mode . "")
+:init (ivy-mode t))
